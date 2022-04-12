278c52dd01ec83e8cb1d7045f750720e27d6f29b
vridhi07 <vridhiw@gmail.com>
2022-04-13 01:04:58 +0530

    event_props

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { PersonList } from "./components/PersonLists";
 import { Status } from "./components/Status";
 import { Heading } from "./components/Heading";
 import { Oscar } from "./components/oscar";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 function App() {
   const personName = {
@@ -36,6 +38,12 @@ function App() {
       <Oscar>
         <Heading>placeholder text</Heading>
       </Oscar>
+      <Input value="" handleChange={(event) => console.log(event)} />
+      <Button
+        handleclick={(event, id) => {
+          console.log(event, id, "button clicked");
+        }}
+      ></Button>
     </div>
   );
 }
