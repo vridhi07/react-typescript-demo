3374ad2e493b8ac06b9b9679c5c0ee22706fd64a
vridhi07 <vridhiw@gmail.com>
2022-04-17 20:53:02 +0530

    props_type

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,9 +4,9 @@ type InputProps = {
   value: string;
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
-export const Input = (props: InputProps) => {
+export const Input = ({ value, handleChange }: InputProps) => {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event);
   };
-  return <input type="text" value={props.value} onChange={handleInputChange} />;
+  return <input type="text" value={value} onChange={handleChange} />;
 };
